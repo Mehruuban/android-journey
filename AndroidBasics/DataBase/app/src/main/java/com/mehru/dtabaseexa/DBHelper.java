391d68955db2e08aebcfda7f7ddd26b509e03bb7
Mehruuban <developermehru@gmail.com>
2025-02-14 22:27:58 +0530

    added basic android projects

--- AndroidBasics/DataBase/app/src/main/java/com/mehru/dtabaseexa/DBHelper.java
+++ AndroidBasics/DataBase/app/src/main/java/com/mehru/dtabaseexa/DBHelper.java
@@ -16,6 +16,7 @@ public class DBHelper  extends SQLiteOpenHelper  {
     private static final String DATA_BASE = "ContactDB";
     private static final int DATABASE_VERSION = 1 ;
     private static final String CONTACT_TABLE = "contacts";
+    //private static Integer id;
     private  static final String KEY_ID = "id";
     private static final String KEY_NAME = "Name";
     private static final String KEY_CONTACT_NO = "ContactNo";
@@ -27,14 +28,20 @@ public class DBHelper  extends SQLiteOpenHelper  {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL("Create Table "+ CONTACT_TABLE +
-                        "(" + KEY_ID + "Integer PRIMARY KEY AUTOINCREMENT," + KEY_NAME + "TEXT," + KEY_CONTACT_NO + "TEXT" + ")" );
+//        database.execSQL("CREATE TABLE "+ CONTACT_TABLE +
+//                        "(" + KEY_ID + "INTEGER PRIMARY KEY AUTOINCREMENT ," + KEY_NAME + "TEXT," + KEY_CONTACT_NO + "TEXT" + ")" );
+//
+//
+//        //to open database
+//        SQLiteDatabase database1 = this.getWritableDatabase();
+//        // to close database
+//        database1.close();
+        String CREATE_TABLE = "CREATE TABLE " + CONTACT_TABLE + " ("
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_NAME + " TEXT, "
+                + KEY_CONTACT_NO + " TEXT)";
 
-
-        //to open database
-        SQLiteDatabase database1 = this.getWritableDatabase();
-        // to close database
-        database1.close();
+        database.execSQL(CREATE_TABLE);
 
     }
 
@@ -58,11 +65,46 @@ public class DBHelper  extends SQLiteOpenHelper  {
 
     }
 
-    //fetch the data from SQLite database
+    // To fetch the data from SQLite database
     public ArrayList<contactModel> fetchContact (){
         SQLiteDatabase database = this.getReadableDatabase();
-       Cursor cursor = database.rawQuery("SELECT * FROM " + CONTACT_TABLE,null );
+        Cursor cursor = database.rawQuery("SELECT * FROM " + CONTACT_TABLE,null );
 
+        ArrayList<contactModel> arrayList  = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            contactModel model = new contactModel();
+            model.id = cursor.getInt(0);
+            model.name = cursor.getString(1);
+            model.ContactNo = cursor.getString(2);
+
+            arrayList.add(model);
+
+        }
+        return arrayList;
+    }
+
+    //To update data
+
+    public void updateContact (contactModel contactModel ){
+
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_CONTACT_NO,"2123456789");
+
+        //values.put(KEY_CONTACT_NO, contactModel.ContactNo); //dynamic way
+
+        database.update(CONTACT_TABLE,values,KEY_ID+"="+contactModel.id,null);
+    }
+
+
+    // To delete data
+
+    public  void deleteContact (int id){
+        SQLiteDatabase database =this.getWritableDatabase();
+
+        database.delete(CONTACT_TABLE,KEY_ID+ "?",new String[]{String.valueOf(id)});
     }
 
 
--- AndroidBasics/DataBase/app/src/main/java/com/mehru/dtabaseexa/MainActivity.java
+++ AndroidBasics/DataBase/app/src/main/java/com/mehru/dtabaseexa/MainActivity.java
@@ -1,12 +1,15 @@
 package com.mehru.dtabaseexa;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DBHelper dbHelper = new DBHelper (this);
+               //code to insert data in database
+//         dbHelper.addContacts("rubatoN","8882988023");
+//        dbHelper.addContacts("membrane","8882988024");
+//        dbHelper.addContacts("mehrubaan","8882988025");
+//        dbHelper.addContacts("NehruBan","8882988026");
+//        dbHelper.addContacts("NehruAlam","8882988027");
 
-         dbHelper.addContacts("rubatoN","8882988023");
-        dbHelper.addContacts("membrane","8882988024");
-        dbHelper.addContacts("mehrubaan","8882988025");
-        dbHelper.addContacts("NehruBan","8882988026");
-        dbHelper.addContacts("NehruAlam","8882988027");
+        //code to update data
+
+        contactModel model = new contactModel();
+        model.id= 1;
+        model.name= "mohd";
+        model.ContactNo = "1232223333";
+        dbHelper.updateContact(model);
+
+
+         // code to delete data
+        dbHelper.deleteContact(2);
+
+        // code to fetch data from SQLite database
+        ArrayList<contactModel>  arrayList = dbHelper.fetchContact();
+        for (int i =0 ; i<arrayList.size(); i++)
+            Log.d("CONTACT_INFO","Name:"+arrayList.get(i).name + ",Contact No: "+arrayList.get(i).ContactNo);
 
     }
 }
