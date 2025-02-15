6d189616e8fc7c4f6d93b91945784d1057f8e358
Mehruuban <developermehru@gmail.com>
2025-02-15 22:06:42 +0530

    added basic android projects

---
+++ AndroidBasics/CameraExa/app/src/main/java/com/mehru/cameraexa/MainActivity.java
@@ -0,0 +1,63 @@
+package com.mehru.cameraexa;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int  REQUEST_CODE_FOR_CAMERA = 100;
+
+    Button  btnImage;
+    ImageView btnCamera;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnImage = findViewById(R.id.btnImage);
+        btnCamera = findViewById(R.id.btnCamera);
+
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(iCamera,REQUEST_CODE_FOR_CAMERA);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode==RESULT_OK){
+            if (requestCode==REQUEST_CODE_FOR_CAMERA){
+                //FOR CAMERA
+              Bitmap bitmap = (Bitmap) (data.getExtras().get("data"));
+                btnCamera.setImageBitmap(bitmap);
+
+            }
+        }
+    }
+}
