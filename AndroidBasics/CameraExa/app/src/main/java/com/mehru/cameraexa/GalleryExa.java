a233b9dcc5ea4ba2e2da351621d2ce9bf7bbc4e7
Mehruuban <developermehru@gmail.com>
2025-02-15 23:10:11 +0530

    added basic android projcts

---
+++ AndroidBasics/CameraExa/app/src/main/java/com/mehru/cameraexa/GalleryExa.java
@@ -0,0 +1,63 @@
+package com.mehru.cameraexa;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GalleryExa extends AppCompatActivity {
+    private final int GALLERY_REQ_CODE = 102 ;
+
+     Button btnGallery;
+     ImageView imgView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_gallery_exa);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnGallery = findViewById(R.id.btnGallery);
+        imgView = findViewById(R.id.imgView);
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery,GALLERY_REQ_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode==RESULT_OK){
+            if (requestCode==GALLERY_REQ_CODE){
+                //FOR GALLERY
+
+                imgView.setImageURI(data.getData());
+            }
+        }
+    }
+}
