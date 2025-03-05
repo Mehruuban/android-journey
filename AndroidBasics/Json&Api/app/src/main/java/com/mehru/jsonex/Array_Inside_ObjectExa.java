3be50a0b3996119622edf0b9aa3a69ea656bff26
Mehruuban <developermehru@gmail.com>
2025-03-06 00:28:45 +0530

    basic android projects

--- AndroidBasics/Json&Api/app/src/main/java/com/mehru/jsonex/Array_Inside_ObjectExa.java
+++ AndroidBasics/Json&Api/app/src/main/java/com/mehru/jsonex/Array_Inside_ObjectExa.java
@@ -34,18 +34,26 @@ public class Array_Inside_ObjectExa extends AppCompatActivity {
         });
 
         Gson gson = new Gson();
-        List<Course2> course2List = new ArrayList<>();
+//        List<Course2> course2List = new ArrayList<>();
+//
+//        course2List.add(new Course2("java",2989));
+//        course2List.add(new Course2("kotlin",2990));
+//        course2List.add(new Course2("android",2991));
+//
+//        Student3 student3 = new Student3("mehru","mehru@gmail.com",23,course2List);
+//
+//           //serialization
+//        String json = gson.toJson(student3);
 
-        course2List.add(new Course2("java",2989));
-        course2List.add(new Course2("kotlin",2990));
-        course2List.add(new Course2("android",2991));
+
+        //deSerialization
+
+        String json_data = "{\"age\":23,\"email\":\"mehru@gmail.com\",\"list\":[{\"course_name\":\"java\",\"fees\":2989},{\"course_name\":\"kotlin\",\"fees\":2990},{\"course_name\":\"android\",\"fees\":2991}],\"name\":\"mehru\"}";
+
+        Student3 student3 = gson.fromJson(json_data,Student3.class);
 
 
-        Student3 student3 = new Student3("mehru","mehru@gmail.com",23,course2List);
-
-           //serialization
-        String json = gson.toJson(student3);
-        Log.d("Test" ,json);
+        Log.d("Test" ,student3.toString());
 
 
 
