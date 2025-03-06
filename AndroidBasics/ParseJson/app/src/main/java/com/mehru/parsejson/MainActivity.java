cbaa92e0af399a5a18d340eccadddb3afd1a222b
Mehruuban <developermehru@gmail.com>
2025-03-06 22:18:28 +0530

    basic android projects

---
+++ AndroidBasics/ParseJson/app/src/main/java/com/mehru/parsejson/MainActivity.java
@@ -0,0 +1,67 @@
+package com.mehru.parsejson;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView textView = findViewById(R.id.TextView);
+        Button button = findViewById(R.id.button);
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Url = "http: //www.json-generator.com/api/json/get/cpvmkwzHkO?indent=2";
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, Url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                requestQueue.add(jsonObjectRequest);
+
+            }
+        });
+
+
+    }
+}
--- AndroidBasics/ParseJson/settings.gradle.kts
+++ AndroidBasics/ParseJson/settings.gradle.kts
@@ -19,7 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "JsonEx"
-include(":app")
+rootProject.name = "ParseJson"
 include(":app")
  
