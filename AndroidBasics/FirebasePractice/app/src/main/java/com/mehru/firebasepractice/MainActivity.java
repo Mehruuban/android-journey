9e637343d22217f7742753e3833396915a635d42
Mehruuban <developermehru@gmail.com>
2025-05-25 21:24:47 +0530

    Firebase exer

---
+++ AndroidBasics/FirebasePractice/app/src/main/java/com/mehru/firebasepractice/MainActivity.java
@@ -0,0 +1,59 @@
+package com.mehru.firebasepractice;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    Button addBtn;
+    EditText edtName ;
+    EditText edtEmail;
+    FirebaseDatabase database;
+    DatabaseReference mRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        addBtn  = findViewById(R.id.addBtn);
+        edtName = findViewById(R.id.editName);
+        edtEmail = findViewById(R.id.editEmail);
+
+//        database = FirebaseDatabase.getInstance();
+//        mRef = database.getReference("message");
+//        mRef.setValue("Meharban alam is a android app developr");
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString();
+                String email = edtEmail.getText().toString();
+            }
+        });
+
+
+    }
+}
