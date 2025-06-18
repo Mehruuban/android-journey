7022e8b8f4a635ee5181e4b9f6832b204270da1a
Mehruuban <developermehru@gmail.com>
2025-06-18 22:00:40 +0530

    view binding

--- app dev kt/ViewBinding/app/src/main/java/com/mehru/viewbinding/MainActivity.kt
+++ app dev kt/ViewBinding/app/src/main/java/com/mehru/viewbinding/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.mehru.viewbinding
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,9 +21,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnSubmit.setOnClickListener {
+            val name = binding.etUser.text.toString()
+            val email = binding.etEmail.text.toString()
 
+            if (binding.checkBox.isChecked){
+                // open new screen
 
-        binding.tv.text = "Mohammad meharban"
+            } else{
+                binding.checkBox.buttonTintList = ColorStateList.valueOf(Color.RED)
+                Toast.makeText(this,"Accept terms and conditions" , Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
